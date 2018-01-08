af087e8387b5af5685a22958839be58c039e8e44
Diak <Diak@T018CG13TEACH>
2018-01-08 17:37:34 +0100

    NEV, message

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
 
-$nev = 'NArnold';
+$nev = 'Kicsi Vuk';
 $datum = new \DateTime();
 $datumStr = $datum->format('Y-m-d\TH:i:s');
 $szin = '#aa5511';
+$szin = '#dd22dd';
 
 require 'contents.php';
